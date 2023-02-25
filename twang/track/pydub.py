c9940e0e23c46ccf1fc7febe14ba61c62c6d383a
mhsb <michael.h.s.ball@gmail.com>
2023-02-25 15:13:45 +0100

    Clean `BaseTrack` & `PyDubTrack` implementations (#4)

--- twang/track/pydub.py
+++ twang/track/pydub.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Union
 
 import pydub
 
@@ -12,17 +12,15 @@ from twang.util import time as time_util
 class PyDubTrack(BaseTrack):
     y: pydub.AudioSegment
 
-    def __init__(self, y: pydub.AudioSegment, sr: Optional[int] = None, audio_format: Optional[AudioFormat] = None):
+    def __init__(self, y: pydub.AudioSegment, sr: Optional[int] = None):
         self.y = y
-        self.sr = sr
-        self.audio_format = audio_format
+        self.sr = y.frame_rate
 
     @classmethod
-    def from_file(cls, file_path: str, audio_format: Optional[AudioFormat] = None):
-        audio_format_str = None if audio_format is None else audio_format.name.lower()
-        audio_segment = pydub.AudioSegment.from_file(file_path, format=audio_format_str)
+    def from_file(cls, file_path: str, audio_format: AudioFormat = AudioFormat.NONE):
+        audio_segment = pydub.AudioSegment.from_file(file_path, format=audio_format.value)
         # TODO: can I also extract the sample rate? (is it 'sample_width')
-        return cls(y=audio_segment, audio_format=audio_format)
+        return cls(y=audio_segment)
 
     @classmethod
     def from_file_snippet(
@@ -30,17 +28,22 @@ class PyDubTrack(BaseTrack):
         file_path: str,
         start_time: float,
         end_time: float,
-        audio_format: Optional[AudioFormat] = None,
+        audio_format: AudioFormat = AudioFormat.NONE,
     ):
         track = cls.from_file(file_path, audio_format=audio_format)
         track.y = track.y[int(time_util.s_to_ms(start_time)) : int(time_util.s_to_ms(end_time))]
         return track
 
-    def save(self, save_path: str):
-        self.y.export(save_path, format=self.audio_format or "wav")
+    def _repr_html_(self) -> str:
+        return self.y._repr_html_()
 
-    def display(self) -> pydub.AudioSegment:
-        return self.y
+    def __getitem__(self, ms_or_slice: Union[int, float, slice]):
+        """TODO: test & document"""
+        self.y = self.y[ms_or_slice]
+        return self
+
+    def save(self, save_path: str, audio_format: AudioFormat = AudioFormat.WAV):
+        self.y.export(save_path, format=audio_format.value)
 
     @property
     def duration(self):
