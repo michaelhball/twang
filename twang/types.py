fb9573732a85a5aaf670f297a7c5a8dc4fbc9162
mhsb <michael.h.s.ball@gmail.com>
2023-12-17 18:24:12 +0100

    Adds JSON type (#14)

--- twang/types.py
+++ twang/types.py
@@ -1,4 +1,5 @@
 import enum
+from typing import List, Mapping, Union
 
 
 class AudioFormat(enum.Enum):
@@ -7,3 +8,6 @@ class AudioFormat(enum.Enum):
     OGG = "ogg"
     FLAC = "flac"
     MP3 = "mp3"
+
+
+JSON = Union[str, int, float, bool, None, Mapping[str, "JSON"], List["JSON"]]
